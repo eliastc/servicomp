017437764c1d65f1f39197232f6a83b9802a8831
Elias Teotonio <eliascalado@hotmail.com>
2021-12-02 10:53:42 -0300

    Projeto criado

---
+++ src/main/java/com/mpinfo/servicosprof/ServicosprofApplication.java
@@ -0,0 +1,13 @@
+package com.mpinfo.servicosprof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicosprofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicosprofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpinfo/servicosprof/ServicosprofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpinfo.servicosprof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosprofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
